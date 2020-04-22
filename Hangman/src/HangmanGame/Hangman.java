2853b027749b07a8f82da789da839c9ed9bbc957
Xueqi Wang <55414437+xueqiwang0v0@users.noreply.github.com>
2020-04-21 21:21:05 -0400

    Update Hangman.java

--- Hangman/src/HangmanGame/Hangman.java
+++ Hangman/src/HangmanGame/Hangman.java
@@ -2,6 +2,10 @@ package HangmanGame;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * This class describes a general word guess game hangman.
@@ -12,7 +16,7 @@ public abstract class Hangman {
 	/**
 	 * "cleanWords": a String list of available words.
 	 */
-	private List<String> cleanWords = new ArrayList<>();
+	public List<String> cleanWords = new ArrayList<>();
 	
 	/**
 	 * "guesses": the times user guesses.
@@ -22,17 +26,30 @@ public abstract class Hangman {
 	/**
 	 * "mistakes": the times user guesses wrong.
 	 */
-	private int mistakes;
+	public int mistakes;
 	
 	/**
 	 * "knowWord": the known part of the word.
 	 */
-	private String[] knownWord;
+	public String[] knownWord;
 	
 	/**
 	 * "correct": an array of booleans that represents whether the character has been guessed or not.
 	 */
-	private boolean[] correct;
+	public boolean[] correct;
+	
+	/**
+	 * This constructor initializes a hangman game and the words list.
+	 * @param path of the txt file
+	 */
+	Hangman(String path){
+		
+		// get the clean words
+		this.cleanWords = this.getCleanWords(path);
+		
+		// set the counters
+		this.guesses = 0;
+	}
 	
 	// Method
 	/**
@@ -41,7 +58,28 @@ public abstract class Hangman {
 	 * @return words (a String List)
 	 */
 	public List<String> getCleanWords(String path){
+		
 		List<String> words = new ArrayList<>();
+		
+		// read the file
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			// read each line of the file
+			String line = br.readLine().strip();
+			while(line != null) {
+				// add the word to the list if it is clean
+				if(this.isClean(line) == true)
+					words.add(line);
+				// read next line
+				line = br.readLine();
+			}
+			
+			br.close();
+			
+		} catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+
 		return words;
 	}
 	
@@ -51,23 +89,82 @@ public abstract class Hangman {
 	 * @return clean (true/false)
 	 */
 	private boolean isClean(String word) {
-		boolean clean;
+		boolean clean = true;
+		// check upper case letters: Zambia
+		if(Character.isUpperCase(word.charAt(0)) == true)
+			clean = false;
+		// check abbreviations: mrs.
+		else if(word.contains("."))
+			clean = false;
+		// check apostrophe: you're
+		else if(word.contains("'"))
+			clean = false;
+		// check hyphen: user-generated
+		else if(word.contains("-"))
+			clean = false;
+		// check compound words: post office
+		else if(word.contains(" "))
+			clean = false;
+		// check digit: 3D
+		else if(this.ContainsDigit(word))
+			clean = false;
 		return clean;
 	}
 	
 	/**
-	 * This method gets the length of the word to guess.
-	 * @return length (int)
+	 * This methods returns the type of current game.
+	 * @return the type of hangman game
 	 */
-	public abstract int getLength() {
-		
+	abstract String getGameType();
+	
+	// helper
+	/**
+	 * The method checks whether a String contains a digit.
+	 * @param word
+	 * @return true/false
+	 */
+	boolean ContainsDigit(String word) {
+		// iterate each character
+		for(int i=0; i<word.length(); i++) {
+			// check if it is a digit
+			if(Character.isDigit(word.charAt(i)) == true)
+				return true;
+		}
+		return false;
 	}
 	
 	/**
 	 * This method print the known word.
 	 */
 	void printKnownWord() {
-		System.out.println(this.knownWord);
+		for(int i=0; i<this.knownWord.length; i++)
+			System.out.print(this.knownWord[i]);
+		System.out.print("\n");
+	}
+	
+	/**
+	 * This method returns what user guesses and check whether it is valid.
+	 * @param scan
+	 * @return the character that user guesses
+	 */
+	String getUserGuess(Scanner scan) {
+		
+		System.out.println("Guess a letter.");
+		
+		String s = scan.next();
+		String ch = Character.toString(s.charAt(0));
+		
+		// check whether the input is valid;
+		while(this.isClean(ch) == false) {
+			System.out.println("Invalid guess, please enter another guess.");
+			s = scan.next();
+			ch = Character.toString(s.charAt(0));
+		}
+		
+		// update the guesses
+		this.guesses += 1;
+				
+		return ch;
 	}
 	
 	/**
@@ -86,5 +183,17 @@ public abstract class Hangman {
 		}
 		
 		return over;
+	}
+	
+	/**
+	 * This method shows the game results.
+	 */
+	void showFinalResults() {
+		// show the game is over
+		System.out.println("The " + this.getGameType() + " hangman game is over.");
+		
+		// show the counters
+		System.out.println("You guess " + this.guesses + " time(s).");
+		System.out.println("You make " + this.mistakes + " mistake(s).");
 	}
 }
